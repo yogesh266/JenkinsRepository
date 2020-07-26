e8d136f0be6fdc6f72f8ac3247448595c50f7151
yogesh <Yogesh.Kumawat@dish.com>
2020-07-26 15:34:02 +0530

    added java program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+
+	public static void printNumbers(){
+		for(int i=0; i<10; i++)
+		System.out.println(i);
+	}
+    public static void main(String args[]) {
+        printNumbers();
+    }
+}
